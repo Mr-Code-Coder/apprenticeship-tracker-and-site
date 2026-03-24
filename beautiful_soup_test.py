b2f9c6ee32288431d23d9bb72d9b527072b9a8bc
Mr-Code-Coder <dodgyfish101@gmail.com>
2026-03-24 16:40:22 +0000

    Writing to CSV
    In this commit I alter the code to open a CSV file and then loop through the books adding them to the file.

--- beautiful_soup_test.py
+++ beautiful_soup_test.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 url = "https://books.toscrape.com"
 response = requests.get(url)
@@ -9,9 +10,11 @@ print(response.status_code) # 200 means can get in whereas 403 means you can't
 soup = BeautifulSoup(response.text, 'html.parser')
 books = soup.find_all("article", class_="product_pod") # finds all <article> tags with the class "product_pod"
 
-for book in books: # loops through all books displayed and gets the title and price
-    title = book.h3.a["title"] # <h3> <a title="..."> </h3> this sets to title down to what shows the title
-    price = book.find("p", class_="price_color").text
-    print(f"{title} -- {price}")
+with open("books.csv", "w", newline="") as file: # opens the "books.csv file"
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Price"])
 
-
+    for book in books: # loops through all books displayed and gets the title and price
+        title = book.h3.a["title"] # <h3> <a title="..."> </h3> this sets to title down to what shows the title
+        price = book.find("p", class_="price_color").text
+        writer.writerow([title, price]) # writes title and price to the csv file
