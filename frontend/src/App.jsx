bd1968b049b3cfe7d1faa22d0e7461ad58a64546
Mr-Code-Coder <dodgyfish101@gmail.com>
2026-03-26 19:03:40 +0000

    Removed unnecessary tags
    In this commit I removed unnecessary tags and added some style.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,12 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)  // allows changing the state of count
-  const [colour, setColour] = useState("red")
   const [jobs, setJobs] = useState([])
 
   useEffect(() => {
-    axios.get('https://apprenticeship-backend.onrender.com/jobs')
-    .then(response => setJobs(response.data))
-    .catch(error => 
+    axios.get('https://apprenticeship-backend.onrender.com/jobs') // query api
+    .then(response => setJobs(response.data)) // then set jobs accordingly
+    .catch(error => // if api is not running fill with temp data
       {console.log("Backend not detected, loading demo data...");
       setJobs([
         { id: 1, title: "Software Engineer Apprentice (Demo)", employer: "Google", location: "London", apply_date: "2026-12-01" },
@@ -20,26 +18,19 @@ function App() {
     });
   }, [])
 
-  useEffect(() => {  // use effect triggers every render by default
-    let timer = setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 
-    5000); // 5 second delay
-    
-    return () => clearTimeout(timer)
-
-  }, [] // Empty array here makes it so it only triggers after the first render
-  )
-
   return ( // what html should fill the container
     <>
       <h1>Apprenticeship Tracking Site</h1>
 
       <div style={{display: 'grid', gap: '20px'}}>
-        {jobs.map((job) => (
-          <div>
-            <p>{job.title}</p>
-            <p>{job.apply_date}</p>
+        {jobs.map((job) => ( // for each job create this div element filled with its values
+          <div className='display_block'>
+            <h4>{job.title}</h4>
+            <h5>{job.employer}</h5>
+            <p>Apply date | Start Date</p>
+            <p>{job.apply_date} | {job.start_date}</p>
+            <a href='{job.link}'>Visit Vacancy</a>
+
           </div>
         ))}
       </div>
